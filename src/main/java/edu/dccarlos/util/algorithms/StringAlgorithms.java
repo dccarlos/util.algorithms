9eabc1a8088f198ba8bdc46de1bd8adc4731b64c
Carlos D. <ccordero@homeaway.com>
2017-05-06 07:32:35 -0500

    Efficient suffix array algorithm

--- src/main/java/edu/dccarlos/util/algorithms/StringAlgorithms.java
+++ src/main/java/edu/dccarlos/util/algorithms/StringAlgorithms.java
@@ -1,5 +1,6 @@
 package edu.dccarlos.util.algorithms;
 
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.Map;
@@ -45,12 +46,55 @@ public class StringAlgorithms {
 	}
 	
 	// https://www.hackerrank.com/challenges/ashton-and-string/topics/lcp-array
-	public static final int[] getSufixArray(final String strArray) {
+	public static final int[] getSuffixArrayNaive(final String strArray) {
 		return IntStream.range(0, strArray.length()).boxed().sorted((x, y) -> {
 			String subStr1 = strArray.substring(x);
 			String subStr2 = strArray.substring(y);
 
 			return subStr1.compareTo(subStr2);
 		}).mapToInt(x -> x).toArray();
+	}
+	
+	public static int[] getSuffixArray(final String str) {
+		final int n = str.length();
+
+		Integer[] order = new Integer[n];
+
+		for(int i = 0; i < n; i++)
+			order[i] = n - 1 - i;
+
+		Arrays.sort(order, (a, b) -> Character.compare(str.charAt(a), str.charAt(b)));
+
+		int[] sa = new int[n];
+		int[] classes = new int[n];
+
+		for(int i = 0; i < n; i++) {
+			sa[i] = order[i];
+			classes[i] = str.charAt(i);
+		}
+
+		for(int len = 1; len < n; len *= 2) {
+			int[] c = classes.clone();
+
+			for(int i = 0; i < n; i++) {
+				classes[sa[i]] = i > 0 && c[sa[i - 1]] == c[sa[i]] && sa[i - 1] + len < n && c[sa[i - 1] + len / 2] == c[sa[i] + len / 2] ? classes[sa[i - 1]] : i;
+			}
+
+			int[] cnt = new int[n];
+
+			for(int i = 0; i < n; i++)
+				cnt[i] = i;
+
+			int[] s = sa.clone();
+
+			for(int i = 0; i < n; i++) {
+				int s1 = s[i] - len;
+
+				if(s1 >= 0)
+					sa[cnt[classes[s1]]++] = s1;
+			}
+		}
+
+		return sa;
 	}
 }
